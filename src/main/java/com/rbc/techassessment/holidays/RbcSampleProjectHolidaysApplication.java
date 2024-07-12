a2d416e3ce6e0701762e7ade64fb58ca851f952d
rahul7a <myemail@domain.com>
2024-07-12 10:19:22 -0400

    Initial commit.

---
+++ src/main/java/com/rbc/techassessment/holidays/RbcSampleProjectHolidaysApplication.java
@@ -0,0 +1,13 @@
+package com.rbc.techassessment.holidays;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RbcSampleProjectHolidaysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RbcSampleProjectHolidaysApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rbc/techassessment/holidays/RbcSampleProjectHolidaysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rbc.techassessment.holidays;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RbcSampleProjectHolidaysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
